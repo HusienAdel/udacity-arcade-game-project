21138ab31df6546ebbaedddcadca89c1d06b73a3
Jan <mail@janschroeder.eu>
2018-06-01 02:44:43 +0200

    working on the enemy factory function. spawning works, y-axis positioning is still off

--- js/app.js
+++ js/app.js
@@ -1,95 +1,115 @@
 //TODO Get the enemy going!
 
 let win = false;
-
+let gameStarted = true
+let randomInteger = function(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 // Enemies our player must avoid
-const Enemy = function() {
+class Enemy {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = 0;
-    this.y = 200;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    constructor() {
+      this.sprite = 'images/enemy-bug.png';
+      this.x = -100;
+      this.y = randomInteger(3) * 100;
+      this.speed = randomInteger(10);
+      this.name = `enemy${enemyCount}`;
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        this.x += (505 / this.speed) * dt;
+
+        if (this.x > 600) {
+          allEnemies = allEnemies.filter(enemy => enemy.name != this.name);
+
+
+        }
+    }
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 };
 
 // Now write your own player class
-const Player = function() {
-  this.sprite = 'images/char-boy.png';
-  this.x = 200;
-  this.y = 400;
-};
 // This class requires an update(), render() and
 // a handleInput() method.
-Player.prototype.update = function(dt){
-
-  if (player.y === 0) {
-
-    win = true;
-
-    setTimeout(function() {
-      reset();
-    }, 1000);
-  }
-
-};
-
-Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Player.prototype.handleInput = function(e){
-
-  if (win === false)  { // Only allow movement as long as the winning condition is not met.
-
-    if (e === 'left' && player.x > 0) {
-      player.x = player.x -= 100;
-      console.log(`player.x: ${player.x}`);
+class Player {
+    constructor() {
+      this.sprite = 'images/char-boy.png';
+      this.x = 200;
+      this.y = 400;
     }
 
-    if (e === 'right' && player.x < 400) {
-      player.x = player.x += 100;
-      console.log(`player.x: ${player.x}`);
+    update(dt){
+
+      if (player.y === 0) {
+        win = true;
+        setTimeout(function() {
+          reset();
+        }, 1000);
+      }
     }
 
-    if (e === 'up' && player.y > 0) {
-      player.y = player.y -= 100;
-      console.log(`player.y: ${player.y}`);
-    };
+    render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 
-    if (e === 'down' && player.y < 400) {
-      player.y = player.y += 100;
-      console.log(`player.y: ${player.y}`);
-    };
-  };
+    handleInput(e){
+
+      if (win === false)  { // Only allow movement as long as the winning condition is not met.
+
+        if (e === 'left' && player.x > 0) {
+          player.x = player.x -= 100;
+        }
+        if (e === 'right' && player.x < 400) {
+          player.x = player.x += 100;
+        }
+        if (e === 'up' && player.y > 0) {
+          player.y = player.y -= 100;
+        }
+        if (e === 'down' && player.y < 400) {
+          player.y = player.y += 100;
+        }
+      };
+    }
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-const allEnemies = [
-];
+
 // Place the player object in a variable called player
 const player = new Player();
+//const bug = new Enemy();
 
+let allEnemies = [];
 
+let enemyCount = 0;
+
+function spawnEnemies() {
+  enemyCount++;
+  const enemy = new Enemy(enemyCount);
+  allEnemies.push(enemy);
+
+  if (enemyCount >= 10) {
+    enemyCount = 0;
+  };
+};
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
 
     var allowedKeys = {
         37: 'left',
@@ -107,3 +127,7 @@ function reset() {
   player.x = 200;
   player.y = 400;
 };
+
+if (gameStarted === true) {
+  spawnTime = setInterval(spawnEnemies, 2000);
+}
