22654b4cd322bad8264b1c9459f12de22176a15b
Jan <mail@janschroeder.eu>
2018-06-01 03:18:00 +0200

    working on the enemy factory function. spawning works, y-axis positioning is still off

--- js/app.js
+++ js/app.js
@@ -1,42 +1,33 @@
-//TODO Get the enemy going!
+//TODO implement collision 
 
 let win = false;
-let gameStarted = true
+let gameRunning = true
 let randomInteger = function(max) {
   return Math.floor(Math.random() * Math.floor(max));
 };
+let allEnemies = [];
+let enemyCount = 0;
+
 // Enemies our player must avoid
 class Enemy {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     constructor() {
       this.sprite = 'images/enemy-bug.png';
       this.x = -100;
-      this.y = randomInteger(3) * 100;
+      //TODO align the positioning on the y-axis with the tiles of the canvas => shuffled array, pick first
+      this.y = (randomInteger(3) + 1) * 100;
       this.speed = randomInteger(10);
       this.name = `enemy${enemyCount}`;
     }
 
-    // Update the enemy's position, required method for game
-    // Parameter: dt, a time delta between ticks
     update(dt) {
+        this.x += (this.speed * 50) * dt;
 
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
-        // all computers.
-        this.x += (505 / this.speed) * dt;
-
-        if (this.x > 600) {
+        if (this.x > 600) { // take the enemies from the array once they are off canvas.
           allEnemies = allEnemies.filter(enemy => enemy.name != this.name);
-
-
         }
     }
 
-    // Draw the enemy on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
@@ -86,16 +77,7 @@ class Player {
     }
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-
-// Place the player object in a variable called player
 const player = new Player();
-//const bug = new Enemy();
-
-let allEnemies = [];
-
-let enemyCount = 0;
 
 function spawnEnemies() {
   enemyCount++;
@@ -124,10 +106,11 @@ document.addEventListener('keydown', function(e) {
 //reset the game to the initial state
 function reset() {
   win = false;
+  gameRunning = false;
   player.x = 200;
   player.y = 400;
 };
 
-if (gameStarted === true) {
+if (gameRunning === true) {
   spawnTime = setInterval(spawnEnemies, 2000);
 }
