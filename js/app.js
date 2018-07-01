02434691f9a64fde68f0d7c3c140437c5856ccd4
husien adel <husien@gmail.com>
2018-07-01 21:20:41 +0300

    update js comments & optimize

--- js/app.js
+++ js/app.js
@@ -4,35 +4,39 @@
 *************************************************s
   */
 
-// Draw Enemies and the Player onto the screen > no separate render() methods required for player and enamy classes
-Object.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
 //  ENEMY CLASS
 
 // Enemies our player must avoid
 var Enemy = function(x, y) {
-  // The image/sprite for our enemies, this uses (helper method provided to easily load images)
+  // The image/sprite for our enemies
   this.sprite = 'images/enemy-bug.png';
-  // Variables applied to each of our instances
   this.x = x;
   this.y = y;
   this.velocity = 100 + Math.floor(Math.random() * 150);
 };
 
-// Update the enemy's position (loop when off canvas)
-// Multiply any movement by the dt parameter (time delta between ticks), this ensures the game runs at the same speed for all computers.
+
+
+
+// Draw Enemies and the Player onto the screen
+Object.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+// Update the enemy's position
+// Multiply any movement by the dt
 Enemy.prototype.update = function(dt) {
   this.x = this.x + this.velocity * dt;
-  this.collide(player);
+  this.collision(player);
   if (this.x > 1500) {
     this.x = -200;
   }
 };
 
-// Reset player to origo when colliding with an enemy
-Enemy.prototype.collide = function(player) {
+// Reset player  when collision with an enemy
+Enemy.prototype.collision = function(player) {
   if (Math.abs(player.x - this.x) < 80 && Math.abs(player.y - this.y) < 30) {
     player.reset();
   }
@@ -82,11 +86,11 @@ Player.prototype.handleInput = function (userInput) {
   }
 };
 
-// Update method for player
+// Update  player method
 Player.prototype.update = function () {
 };
 
-// Reset Player to start position when reaching water or colliding with enemy
+// Reset Player to start position when reaching water or collision with enemy
 Player.prototype.reset = function() {
   player.x = 200;
   player.y = 400;
